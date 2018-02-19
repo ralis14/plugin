c6e05c4b812b93ac39ff0ba2f9176b13994abc26
ralis14 <rula018@gmail.com>
2018-02-18 21:55:27 -0700

    Added some files for testing run python Docker.py on folder interfaces, then see the image using shell command docker ps -a, cannot get volume mounting to work

--- var/interfaces/Docker.py
+++ var/interfaces/Docker.py
@@ -1,17 +1,48 @@
 import docker
 import os
-
+from pathlib import Path
+root_dir = str(Path.cwd())
 
 '''
 add the certs cert.pem and key.pem information or a tls config file with the paths, run docker-machine env on the server to view the url and paths
 '''
 certs = {"cert":str(os.environ['docker_client_cert']), "key":str(os.environ['docker_client_key'])}
-class Docker:
+class DockerApi:
     def __init__(self, url='192.168.99.100:2376', tls_config=docker.tls.TLSConfig(client_cert=(certs['cert'], certs['key']))):
         self.server = docker.from_env()
         self.client = docker.DockerClient(base_url=url, tls=tls_config)
+        self.client_params = {'tty':False, 'exec':'/bin/bash', 'auto_remove':False, 'detach':False, 'name':'default','stdin_open':False, 'volumes':{}}
         pass
-    
+    def ping(self):
+        print(self.client.ping())
+    def tty(self):
+        self.client_params['tty']=True
+        return self
+    def stdinOpen(self):
+        self.client_params['stdin_open']= True
+        return self
+    def detach(self):
+        self.client_params['detach']=True
+        return self
+    def image(self, image):
+        self.client_params['image']= image
+        return self
+    def volumes(self, host_path=root_dir, docker_path='/app', mode='rw'):
+        volumes={host_path:{'bind':docker_path, 'mode':mode}}
+        self.client_params['volumes']=volumes
+        return self
+    def container_name(self, name):
+        self.client_params['name']=name
+        return self
+    def auto_remove(self):
+        self.client_params['auto_remove']=True
+        return self
+    def execute(self, command):
+        self.client_params['exec']=command
+        return self
+    def run(self):
+        cont = self.client.containers.run(self.client_params['image'], auto_remove=True, command=self.client_params['exec'], detach=self.client_params['detach'], tty=self.client_params['tty'], name=self.client_params['name'], stdin_open=self.client_params['stdin_open'], volumes=self.client_params['volumes'])
+
     '''
     Get the list of items make them strings and check if the image
     is in the list
@@ -19,7 +50,7 @@ class Docker:
     def _image(self, image):
         dic = {'<':'','>':'',':':'','Image':''}
         try:
-            im = self.client.images.list()
+            im = self.server.images.list()
         except:
             print("There was an Error")
         for x in im:
@@ -54,3 +85,9 @@ class Docker_String():
     def combine(self):
         li = self.__dict__.values()
         print(' '.join(li))
+
+
+
+d = DockerApi()
+#d.ping();
+cont = d.image('rula018/practicum').detach().tty().stdinOpen().volumes().execute('/bin/zsh').run()
